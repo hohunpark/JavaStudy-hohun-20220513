092c5143fce9520050323220ea9dcfa3928ee2bd
hohunpark <pophh@naver.com>
2022-05-13 21:09:46 +0900

    프로젝트 첫 커밋 변수와 상수 수업 코드

---
+++ JavaStudy_hohun/src/j02_변수와_상수/MyInfo.java
@@ -0,0 +1,43 @@
+package j02_변수와_상수;
+
+public class MyInfo {
+
+	public static void main(String[] args) {
+		// 주석 = 메모
+		/*
+		 * 1. 문제
+		 * name1, name2, name3
+		 * 박, 호, 훈
+		 * age(나이)
+		 * phone(연락처)
+		 * address(주소)
+		 * 
+		 * 이름 : 김준일
+		 * 나이 : 29
+		 * 연락처 : 010-1010-1010
+		 * 주소 : 부산 동래구 사직동
+		 * 
+		 */
+		
+		
+		char name1 = '김';
+		char name2 = '준';
+		char name3 = '일';
+		int age = 29;
+		String phone = "010-1010-1010";
+		String address = "부산 동래구 사직동";
+		
+		System.out.println("이름 : " + name1+name2+name3);
+		System.out.println("나이 : " + age);
+		System.out.println("연락처 : " + phone);
+		System.out.println("주소 : " + address);
+		System.out.println("세수의 합 : " + (10+20+30));
+		
+		
+		
+		
+		
+		
+	}
+
+}
