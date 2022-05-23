0ac1574f951ac8e9f45010ef2b31fe7384219ae6
hohunpark <pophh@naver.com>
2022-05-23 21:53:06 +0900

    클래스 교육

---
+++ JavaStudy_hohun/src/j08_클래스/CarMain.java
@@ -0,0 +1,68 @@
+package j08_클래스;
+
+/*
+ * 1. Car 클래스를 만든다.
+ * 2. Car 클래스의 속성(변수) 제조사(company), 모델(model), color(색상)
+ * 3. Car 클래스의 기능(메소드) showCompanyInfo(
+ * showCompanyInfo() -> 000회사에서 제조된 차량입니다.
+ * showInfoAll() -> 
+ * 제조사 : KIA
+ * 모델 : k5
+ * 색상 : 화이트
+ * getCar() -> return "KIA_K5_화이트"
+ * setColor() -> 매개변수에 색상을 받아서 color 변수의 값을 변경
+ * 
+ * KIA, K7, 블랙 - setColor(블루)
+ * 현대자동차, 소나타, 화이트 - setColor(그레이)
+ * Tesla, model13, 레드 - setColor(그린)
+ * 
+ */
+
+public class CarMain {
+
+	public static void main(String[] args) {
+		
+		Car kcar = new Car();
+		kcar.company = "KIA";
+		kcar.model = "K7";
+		kcar.color = "블랙";
+		
+		Car hcar = new Car();
+		hcar.company = "현대";
+		hcar.model = "소나타";
+		hcar.color = "화이트";
+		
+		Car tcar = new Car();
+		tcar.company = "Tesla";
+		tcar.model = "model13";
+		tcar.color = "레드";
+		
+		
+		
+		kcar.showCompanyInfo();
+		hcar.showCompanyInfo();
+		tcar.showCompanyInfo();
+		
+		kcar.showInfoAll();
+		hcar.showInfoAll();
+		tcar.showInfoAll();
+		
+		System.out.println();
+		
+		System.out.println(kcar.getCar());
+		System.out.println(hcar.getCar());
+		System.out.println(tcar.getCar());
+		
+		System.out.println();
+		
+		kcar.setColor("블루");
+		hcar.setColor("그레이");
+		tcar.setColor("그린");
+		
+		kcar.showInfoAll();
+		hcar.showInfoAll();
+		tcar.showInfoAll();
+		
+	}
+
+}
---
+++ JavaStudy_hohun/src/j08_클래스/FishBun.java
@@ -0,0 +1,22 @@
+package j08_클래스;
+
+public class FishBun {
+	String material; //재료
+	String dough;
+	
+	FishBun() {		//생성자
+		System.out.println("생성자 호출");
+	}
+	
+	void test() {
+		
+	}
+	
+	void showInfo() {
+		System.out.println("재료 : " + material);
+		System.out.println("반죽 : " + dough);
+	}
+	
+	
+
+}
